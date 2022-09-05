6b0ee07a51b612894b2f66e0592da0846be9aa4e
Lyod Rhussel Torrenueva <111472869+MistMe@users.noreply.github.com>
2022-09-05 09:30:34 +0800

    Add files via upload

---
+++ exam_1/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+
+    String surname;
+    String firstname;
+    String middlein;
+    String dateofbirth;
+    int studentno;
+    String studentemail;
+    boolean iamawesome;
+
+    public void sayMyStudentNumber() {
+        System.out.println("My Student Number:" + studentno);
+    }
+
+    public void sayMyEmailAddress() {
+        System.out.println("My Email Address:" + studentemail);
+    }
+
+    public void AmIAwesome() {
+        System.out.println("Am I Awesome:" + iamawesome);
+    }
+
+}
